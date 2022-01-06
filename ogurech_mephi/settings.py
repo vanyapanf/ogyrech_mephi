e392e0dd16351975a4369f53e099df5d9a801597
KamilaMalikova <kamilamalikova173@gmail.com>
2022-01-06 18:11:33 +0300

    Models added

--- ogurech_mephi/settings.py
+++ ogurech_mephi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'service',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- service/models.py
+++ service/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+
+class Release(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    description = models.TextField()
+
+
+class TestPlan(models.Model):
+    release = models.OneToOneField(Release, on_delete=models.CASCADE)
+
+
+class TestCase(models.Model):
+    description = models.TextField()
+    expectedResult = models.CharField(max_length=200)
+
+
+class TestRun(models.Model):
+    testPlan = models.ForeignKey(TestPlan, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    testCase = models.ManyToManyField(TestCase, blank=True)
+
+
+class TestRunResult(models.Model):
+    testRun = models.ForeignKey(TestRun, on_delete=models.CASCADE)
+    testPlan = models.ForeignKey(TestPlan, on_delete=models.CASCADE)
+    runDate = models.DateTimeField()
+    successfulTestes = models.IntegerField()
+    failedTestes = models.IntegerField()
+    progress = models.IntegerField()
+    isFinished = models.BooleanField()
+
+
+class TestCaseResult(models.Model):
+    testCase = models.ForeignKey(TestCase, on_delete=models.CASCADE)
+    testRun = models.ForeignKey(TestRun, on_delete=models.CASCADE)
+    testRunResult = models.ForeignKey(TestRunResult, on_delete=models.CASCADE)
+    isSuccessful = models.BooleanField()
+    runDate = models.DateTimeField()
+    realResult = models.CharField(max_length=200)
+
